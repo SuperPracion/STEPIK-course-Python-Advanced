a31031e9c7e7f5ccb7683e5dae9cb7c349ba6c30
SuperPracion <pracion@bk.ru>
2022-09-22 13:31:09 +0300

    Решение модуля 8.8

---
+++ Section 8.8/2_first_letter_every_word.py
@@ -0,0 +1,4 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+new_letters_set = set(word[0].lower() for word in words)
+
+print(*sorted(new_letters_set))
---
+++ Section 8.8/5_png_format.py
@@ -0,0 +1,4 @@
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+all_png_files = set(word.lower() for word in files if word.lower().count('.png'))
+
+print(*sorted(all_png_files))

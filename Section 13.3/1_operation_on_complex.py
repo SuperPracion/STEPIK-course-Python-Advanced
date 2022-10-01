800aa679cd6338117746954666a5b489ff80a30e
SuperPracion <pracion@bk.ru>
2022-10-01 14:07:07 +0300

    Решение модуля 13.3

---
+++ Section 13.3/1_operation_on_complex.py
@@ -0,0 +1,6 @@
+import cmath
+num1, num2 = complex(input()), complex(input())
+
+print(f'{num1} + {num2} = {num1 + num2}')
+print(f'{num1} - {num2} = {num1 - num2}')
+print(f'{num1} * {num2} = {num1 * num2}')
---
+++ Section 13.3/3_conjugation_nums.py
@@ -0,0 +1,6 @@
+import cmath
+n = int(input())
+num1 = complex(input())
+num2 = complex(input())
+
+print(num1 ** n + num2 ** n + num1.conjugate() ** n + num2.conjugate() ** n + 1)
